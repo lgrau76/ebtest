a0a5c7a8d0d686a9d319d47d81827276f6665004
laurent <lgrau@snopsys.com>
2019-07-12 09:17:11 +0200

    comment tets hadoop_home

--- src/test/scala/com/earlybirds/dflookupTests.scala
+++ src/test/scala/com/earlybirds/dflookupTests.scala
@@ -6,6 +6,31 @@ import org.apache.spark.sql.types.{StructField, StructType, _}
 import org.scalatest.{FunSuite, GivenWhenThen, Inspectors, Matchers}
 import org.apache.spark.sql.functions.expr
 
+/*
+In a FunSuite, tests are function values. (The “Fun” in FunSuite stands for function.)
+You denote tests with test and provide the name of the test as a string enclosed in parentheses,
+followed by the code of the test in curly braces. Here's an example:
+ */
+
+/*
+Using matchers
+ScalaTest provides a domain specific language (DSL) for expressing assertions in tests using the word should. Just mix in
+ */
+
+/* https://github.com/holdenk/spark-testing-base/wiki/DataFrameSuiteBase   !!!!
+DataFrameSuiteBase enables you to check if two DataFrames are equal. It also provides an easy way to get SparkContext and sqlContext. SparkContext and SqlContext are initialized before all testcases, So you can access them inside any test case.
+For Java users the same functionality is supported by JavaDataFrameSuiteBase.
+For Java users the same functionality is supported by JavaDataFrameSuiteBase.
+You can assert the DataFrames equality using method assertDataFrameEquals.
+ */
+
+/* https://github.com/holdenk/spark-testing-base/wiki/SharedSparkContext !!!
+Instead of initializing SparkContext before every test case or per class you can easily get your SparkContext by extending SharedSparkContext.
+SharedSparkContext initializes SparkContext before all test cases and stops this context after all test cases.
+For Spark 2.2 and higher you can also share the SparkContext (and SparkSession if in DataFrame tests)
+between tests by adding override implicit def reuseContextIfPossible: Boolean = true to your test.
+ */
+
 class dflookupTests extends  FunSuite with SharedSparkContext with Matchers with Inspectors with GivenWhenThen with DataFrameSuiteBase {
   lazy implicit val _spark = spark
   import _spark.implicits._
